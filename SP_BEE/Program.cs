e60dd3e2daa11cb5c597ae302744f444bc611310
TanguyGackel <tanguy.gackel@gmail.com>
2025-03-31 22:03:06 +0200

    a

---
+++ SP_BEE/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using SP_BEE;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MSServer msServer = MSServer.Instance;
+
+        msServer.Start(IPAddress.Parse("127.0.0.1"), 9000);
+        
+        
+    }
+}
